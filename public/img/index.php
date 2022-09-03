f9658c87c2f3bd8905a645bee0a61afe844aff27
Mohammad Ibrahim <awesomecoder.dev@gmail.com>
2022-09-04 00:39:48 +0600

    Shortvideo Downloader Production

---
+++ public/img/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// require __DIR__ . '/../../vendor/autoload.php';
+// require __DIR__ . '/../../bootstrap/app.php';
+
+require __DIR__ . '/../../ibrahim/vendor/autoload.php'; //live server
+require __DIR__ . '/../../ibrahim/bootstrap/app.php'; //live server
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+$kernel->terminate($request, $response);
